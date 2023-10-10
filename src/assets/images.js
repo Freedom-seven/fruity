f61a162235f75137f4ecc3cbb7984df0847c1970
FREEDOM <freewhynane62@gmail.com>
2023-10-10 16:30:57 +0000

    import and export images

---
+++ src/assets/images.js
@@ -0,0 +1,61 @@
+import apple from "./popular-1.png";
+import watermelon from "./popular-2.png";
+import pineapple from "./popular-3.png";
+import banana from "./popular-4.png";
+import strawberry from "./popular-5.png";
+import mango from "./popular-6.png";
+import carrot from "./carrots.png";
+import spinach from "./fresh-spinach.png";
+import tomato from "./tomatoes.png";
+import garlic from "./garlic.png";
+import onion from "./red-onion.png";
+import grapes from "./login-fruit.png";
+import orange from "./orange.png";
+import zucchini from "./zucchini.png";
+import bellpepper from "./bellpepper.png";
+import cucumber from "./cucumber.png";
+import broccoli from "./broccoli.png";
+import kiwi from "./kiwi.png";
+import pear from "./pear.png";
+import promo1 from "./fruit1.png";
+import promo2 from "./promo4.png";
+import promo3 from "./promo3.png";
+import promo4 from "./basket-full-vegetables.png";
+import carousel1 from "./carousel1.png";
+import carousel2 from "./carousel2.png";
+import carousel3 from "./carousel3.png";
+import carousel4 from "./carousel4.png";
+import carousel5 from "./carousel5.png";
+import logo from "./logo.png";
+
+export {
+  apple,
+  watermelon,
+  pineapple,
+  banana,
+  strawberry,
+  mango,
+  grapes,
+  promo1,
+  promo2,
+  promo3,
+  promo4,
+  carrot,
+  spinach,
+  tomato,
+  garlic,
+  onion,
+  carousel1,
+  carousel2,
+  carousel3,
+  carousel4,
+  carousel5,
+  orange,
+  zucchini,
+  bellpepper,
+  cucumber,
+  broccoli,
+  kiwi,
+  pear,
+  logo,
+};
